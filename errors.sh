afb79ee83334572b627cc622494643d8055989be
Raúl Heredia <40383817+raul-heredia@users.noreply.github.com>
2021-04-10 15:48:57 +0200

    Added comments to errors.sh

--- errors.sh
+++ errors.sh
@@ -10,35 +10,37 @@ LOGFILE=${DIR}/error.logs
 
 function GET() {
     echo "[..] Comprovant si ${DIR} existeix"
-    if [[ ! -d "${DIR}" ]]; then
+    if [[ ! -d "${DIR}" ]]; then # Comprovem si existeix, si no creem el directori
         mkdir ${DIR} && echo "[OK] ${DIR} S'ha creat correctament." || echo "[ERROR] No s'ha pogut crear ${DIR}"
     else
         echo "[OK] ${DIR} ja existeix."
     fi
+    # Descarreguem l'arxiu
     wget -P ${DIR} https://www.collados.org/asix2/m06/uf2/error.logs >/dev/null 2>&1 && echo "[OK] El fitxer ${LOGFILE} s'ha descarregat correctament." || echo "[ERROR] No s'ha pogut descarregar el fitxer ${LOGFILE}"
 }
 
 function USERINPUT() {
+    # Demanem les dates
     echo -n "Introdueix l'any en format [YYYY]: "
     read YEAR
     echo -n "Introdueix el mes en format [MMM] amb la primera lletra en majúscula: "
     read MONTH
     echo -n "Introdueix el día en format [DD]: "
     read DAY
-    YEARAWK="${YEAR}]"
-    TIMESTAMP=$(echo "${YEAR}${MONTH}${DAY}")
-    OUTPUTFILE="err${TIMESTAMP}.log"
+    TIMESTAMP=$(echo "${YEAR}${MONTH}${DAY}") # Aquest serà el timestamp per a l'arxiu en el que es guardaran
+    OUTPUTFILE="err${TIMESTAMP}.log"          # L'arxiu on aniran guardats sera errYYYYMMDD.log
 }
 function GET_ERRORS() {
-    cd ${DIR}
-    awk -v d="$DAY" -v m="$MONTH" -v y="$YEARAWK" '$2==m && $3==d && $5==y {print $0}' error.logs >logtemp.log
-    awk '$6=="[error]" {print $0}' logtemp.log >${OUTPUTFILE} && echo "[OK] S'han extret els errors de la data ${MONTH} ${DAY} ${YEAR} al fitxer ${DIR}/${OUTPUTFILE}" || echo "[ERROR] No s'han pogut extreure els errors de la data ${MONTH} ${DAY} ${YEAR}"
-    rm logtemp.log
+    cd ${DIR} # Canviem al directori on esta l'arxiu error.logs
+    YEARAWK="${YEAR}]" # Afegim un "]" al final ja que la columna de l'any conté un ] al final i així ens asegurem que no dona errors
+    awk -v d="$DAY" -v m="$MONTH" -v y="$YEARAWK" '$2==m && $3==d && $5==y {print $0}' error.logs >logtemp.log # Agafem tots els registres del dia concret introduït per l'usuari i el guardem en un arxiu temporal anomenat logtemp.log
+    awk '$6=="[error]" {print $0}' logtemp.log >${OUTPUTFILE} && echo "[OK] S'han extret els errors de la data ${MONTH} ${DAY} ${YEAR} al fitxer ${DIR}/${OUTPUTFILE}" || echo "[ERROR] No s'han pogut extreure els errors de la data ${MONTH} ${DAY} ${YEAR}" #Agafem els registres de logtemp.log i agafem nomes els que tinguin "[error]" i el guardem a l'arxiu errYYYYMMMDD.log
+    rm logtemp.log # Esborrem l'arxiu temporal logtemp.log  
     cd ~/
     echo "[..] Nota: Si al dia introduït no va haver-hi cap error el fitxer ${DIR}/${OUTPUTFILE} estarà en blanc."
 }
 function CLEAN() {
-    rm ${LOGFILE}
+    rm ${LOGFILE} # Esborrem l'arxiu descarregat
 }
 function MAIN() {
     USERINPUT
